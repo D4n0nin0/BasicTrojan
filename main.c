499ebac630d4ce06434ef6b6aa4cd9fe943acb77
D4n0nin0 <jdanielov04@gmail.com>
2025-09-01 12:34:28 -0700

    create a program that executes a simple message

--- main.c
+++ main.c
@@ -1,1 +1,10 @@
-"" 
+#include<windows.h>
+#pragma comment (lib, "user32.lib")
+
+int main (VOID){
+    MessageBox(NULL,
+                "The Wold Is Ugly, Im the Darkness",
+                "MZ8K",
+                MB_ICONEXCLAMATION | MB_OK);
+    return 0;
+}
